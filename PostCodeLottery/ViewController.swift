d8b8dff654aa121a6faff00835c5b90342512870
Troy Copeland <Troy1994copeland@aol.com>
2017-07-04 11:04:43 +0100

    Initial Commit

--- PostCodeLottery/ViewController.swift
+++ PostCodeLottery/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton : FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["email", "user_birthday", "public_profile"]
+        
+        //loginButton = LoginButton(readPermissions: [ .PublicProfile, .Email, .UserFriends ])
+        
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        
+        loginButton.delegate = self
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+        print(error)
+        return
+        }
+        
+        print("Successfully logged in with facebook")
     }
 
     override func didReceiveMemoryWarning() {
