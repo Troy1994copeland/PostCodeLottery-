9fef4391f2cdadfa9ab1e24b5676b0123ec2ae4e
Troy Copeland <Troy1994copeland@aol.com>
2017-07-06 12:56:05 +0100

    FB Login and User details posted to HTTP API

--- PostCodeLottery/ViewController.swift
+++ PostCodeLottery/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 import FBSDKLoginKit
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    var email1 : String = ""
+    var firstName1 : String = ""
+    var lastName1 : String = ""
+    var gender1 : String = ""
+    var age1 : String = ""
+    var age_no : NSNumber = 0
+    var id1 : String = ""
+    var locale1 : String = ""
+    var picture1 : NSString = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +36,15 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         if (FBSDKAccessToken.current()) != nil {
         fetchProfile()
         }
+        
+       
     }
     
     func fetchProfile() {
     print("fetchProfile")
         
     
-        FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "email"]).start { (connection, result, error) -> Void in
+        FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "email, first_name, last_name, gender, age_range, id, locale, picture"]).start { (connection, result, error) -> Void in
             
             if error != nil {
             print(error!)
@@ -39,10 +52,88 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             }
             
             if let email = (result as AnyObject)["email"] as? String {
-            print(email)
+                self.email1 = email
+                print(email)
             }
+            
+            if let firstName = (result as AnyObject)["first_name"] as? String {
+               self.firstName1 = firstName
+               print(firstName)
+            }
+            
+            if let lastName = (result as AnyObject)["last_name"] as? String {
+                self.lastName1 = lastName
+                print(lastName)
+            }
+            
+            if let gender = (result as AnyObject)["gender"] as? String {
+                self.gender1 = gender
+                print(gender)
+            }
+            
+            if let age = (result as AnyObject)["age_range"] as? NSDictionary {
+                
+                self.age_no = age["min"] as! NSNumber
+                self.age1 = String(describing: self.age_no)
+                print(self.age1)
+                
+            }
+            
+            if let id = (result as AnyObject)["id"] as? String {
+                self.id1 = id
+                print(id)
+            }
+
+            if let locale = (result as AnyObject)["locale"] as? String {
+                self.locale1 = locale
+                print(locale)
+            }
+
+            if let picture = (result as AnyObject)["picture"] as? NSDictionary {
+                //print(picture)
+                if let data = (picture as AnyObject)["data"] as? NSDictionary {
+                    
+                    self.picture1 = data["url"] as! NSString
+                    print(data["url"] as! String)
+                }
+            }
+            
+           
     }
+        data_request("http://recommended-apps.com/api/validatefb.php")
+        
 }
+   
+    func data_request(_ url:String)
+    {
+        let url:NSURL = NSURL(string: url)!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "POST"
+        
+        let paramString = "email\(email1)&first_name=\(firstName1)&last_name\(lastName1)&id\(id1)&gender\(gender1)&locale\(locale1)&age\(age1)&picture\(picture1)"
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest) {
+            (
+            data, response, error) in
+            
+            guard let _:NSData = data as NSData?, let _:URLResponse = response, error == nil else {
+                print("error")
+                return
+            }
+            
+            if let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            {
+                print(dataString)
+            }
+        }
+        
+        task.resume()
+        
+    }
+
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
         print("Did log out of facebook")
